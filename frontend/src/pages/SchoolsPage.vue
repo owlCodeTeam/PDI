6dccbd4fdabee721ab462dc8f218403051b69bc9
henriqueWebDev <henriquedevprofessional@gmail.com>
2024-01-02 13:58:00 -0400

    Comecando os testes!

--- frontend/src/pages/SchoolsPage.vue
+++ frontend/src/pages/SchoolsPage.vue
@@ -1,12 +1,13 @@
 <template>
   <q-page padding>
     <!-- content -->
+    schools page
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  // name: 'PageName'
+  name: 'SchoolsPage'
 })
 </script>
