d83efe96ac3af72fbc3d69b870d3797e125a3320
Dione Bruno <dionebruno88@gmail.com>
2024-01-03 14:38:15 -0400

    Swagger

--- backend/src/core/infra/http/nestjs/auth/auth.module.ts
+++ backend/src/core/infra/http/nestjs/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
 
 @Module({
-  controllers: [],
+  controllers: [AuthController],
   providers: [],
 })
 export class AuthModule {}
